2c3a393611d73856e6a507fce96c7ebe651d1789
alejandro oñate <alejandro.onate.c@gmail.com>
2023-01-11 12:30:10 -0300

    se agregar el puerto por variable de entorno

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
