af35227533d025543f89833671f64440f4a6b511
alejandro oñate <alejandro.onate.c@gmail.com>
2023-02-13 17:12:39 -0300

    se soliciona problema de nombres de data

--- src/main/services/user.services.ts
+++ src/main/services/user.services.ts
@@ -253,7 +253,7 @@ export class UserServices implements UserServicesInterface {
         name: element.nombre.trim().toLowerCase(),
         email: element.correo.trim().toLowerCase(),
         profile: element.perfil.trim().toLowerCase(),
-        directive: institucionData.sostenedor.name,
+        directive: infoInstitucion.institucion.trim().toLowerCase(),
         rut: element.rut.trim().toLowerCase(),
       };
       await this.saveUser(userData);
