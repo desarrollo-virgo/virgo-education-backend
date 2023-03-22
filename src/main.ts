58cd05f2ca3324c2cf78d5a34dee41623fa92b25
alejandro oñate <alejandro.onate.c@gmail.com>
2023-03-22 00:50:47 -0300

    tiempo de espera en multer

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as timeout from 'connect-timeout';
 
 async function bootstrap() {
   const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  app.use(timeout('600s'));
-  // app.use(haltOnTimedout);
   await app.listen(PORT);
 }
 
-function haltOnTimedout(req, res, next) {
-  if (!req.timedout) next();
-}
 bootstrap();
